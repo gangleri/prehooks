bd9cdba129d50fec2bc3856ab8f86c12f0023559
Alan Bradley <alan@gangleri.net>
2015-09-07 15:37:15 +0100

    getting installer working

--- hook.js
+++ hook.js
@@ -4,21 +4,24 @@
 
 var fs = require('fs')
 var path = require('path')
-var childproc = require('child_Process')
+var childproc = require('child_process')
 
 var pkg = require('../../package.json')
-var hookName = "pre-commit"
+var hookName = path.basename(process.argv[1])
 var hooks = pkg[hookName]
+var numHooks = hooks.length -1
 
 var results = {}
 var complete = 0
 var exitCode = 0
 
-
 var reportSummary = function reportSummary () {
-	if (exitCode === 0) { return }
-}
+	Object.keys(results).forEach(function(r) {
+		console.log('%s\t\t%s', r, results[r])
+	})
 
+	process.exit(exitCode)
+}
 
 // script entry
 if (!hooks || hooks.length === 0) { process.exit(0) }
@@ -29,15 +32,15 @@ hooks.forEach(function (script) {
 
 		if (err) {
 			exitCode = 1
-			results[script] = false
+			results[script] = 'FAIL' 
 			return
 		}
 
-		results[script] = true
+		results[script] = 'PASS'
 		console.log(script)
 		console.log(stdout)
 
-		if (complete === hooks.length -1) { return reportSummary() }
+		if (complete === numHooks) { return reportSummary() }
 	})
 })
 
--- install.js
+++ install.js
@@ -4,18 +4,17 @@ var fs = require('fs')
 var path = require('path')
 var projDir = path.resolve('./', '../..')
 
-console.dir(projDir)
-
-if (fs.existsSync(projDir + '.git') === false) {
+if (fs.existsSync(projDir + '/.git') === false) {
 	console.log('Unable to find .git directory. Unable to install hooks')
 	process.exit(1)
 }
 
-var rs = fs.createReadStream(__dirname + '/hook.js')
-var dest = path.resolve(projDir, '.git', 'hook.js')
-var ws = fs.createWriteStream(dest, {mode: '0755'})
+var hookFile = path.resolve(projDir, '.git', 'hooks', 'hook.js')
+fs.createReadStream(__dirname + '/hook.js').
+	pipe(fs.createWriteStream(hookFile))
 
-['commit-msg', 'pre-commit', 'pre-push'].forEach(function(hook) {
-	fs.fs.symlinkSync(dest, path.resolve(projDir, '.git', hook))
+var hooks = ['commit-msg', 'pre-commit', 'pre-push']
+hooks.forEach(function(hook) {
+	fs.symlinkSync(hookFile, path.resolve(projDir, '.git', 'hooks', hook))
 })
 
